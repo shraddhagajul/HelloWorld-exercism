2d9c672d022673d412031768d4e9ad07889dbdcc
Shraddha Gajul <shraddha.gajul.27@gmail.com>
2020-10-16 09:52:41 +0530

    Working solution

---
+++ hello_world.go
@@ -0,0 +1,11 @@
+// This is a "stub" file.  It's a little start on your solution.
+
+
+// Package greeting should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package greeting
+// HelloWorld -  returns string Hello, World!
+func HelloWorld() string {
+
+	return "Hello, World!";
+}
